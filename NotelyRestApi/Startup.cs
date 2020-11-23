ea06f6e994a5610f09b8e8a0c64d8a83a6494fc7
Miruna Vlc <vlaicu_miruna@yahoo.com>
2020-11-23 20:02:47 +0200

    register db context service

--- NotelyRestApi/Startup.cs
+++ NotelyRestApi/Startup.cs
@@ -1,14 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NotelyRestApi.Database;
+using NotelyRestApi.Repositories;
+using NotelyRestApi.Repositories.Implementations;
 
 namespace NotelyRestApi
 {
@@ -25,6 +23,8 @@ namespace NotelyRestApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<NotelyDbContext>(options => options.UseSqlite(Configuration["Data:NotelyRestApi:ConnectionString"]));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
