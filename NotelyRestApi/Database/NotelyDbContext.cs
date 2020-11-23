fcaa3555ee12c7d0315e91e9dafb6c9977327719
Miruna Vlc <vlaicu_miruna@yahoo.com>
2020-11-23 20:00:47 +0200

    create notely context

---
+++ NotelyRestApi/Database/NotelyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotelyRestApi.Models;
+
+namespace NotelyRestApi.Database
+{
+    public class NotelyDbContext : DbContext
+    {
+        public NotelyDbContext(DbContextOptions<NotelyDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
