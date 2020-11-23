beeb9ab976b8d6f2692df8b98f6eb173ef3bf60e
Miruna Vlc <vlaicu_miruna@yahoo.com>
2020-11-23 20:00:07 +0200

    create note model

---
+++ NotelyRestApi/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotelyRestApi.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; }
+        public string Detail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
